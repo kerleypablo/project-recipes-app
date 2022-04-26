b1a49439ecceff168c4451bffbf9e25548b6df81
MariaJL <mariajr.lisboa@gmail.com>
2022-04-26 18:41:57 -0300

    requisito 20

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
     <footer data-testid="footer">
-      <Link to="/drinks" data-testid="drinks-bottom-btn"><span>Bebidas</span></Link>
-      <Link to="/explore" data-testid="explore-bottom-btn"><span>Explore</span></Link>
-      <Link to="/food" data-testid="food-bottom-btn"><span>Comida</span></Link>
+      <Link to="/drinks" data-testid="drinks-bottom-btn">
+        <img src={ drinkIcon } alt="drink Icon" />
+      </Link>
+      <Link to="/explore" data-testid="explore-bottom-btn">
+        <img src={ exploreIcon } alt="explore Icon" />
+      </Link>
+      <Link to="/food" data-testid="food-bottom-btn">
+        <img src={ mealIcon } alt="meal Icon" />
+      </Link>
     </footer>
   );
 }
