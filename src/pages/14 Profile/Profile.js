a3f372a7687d360745987a9baacb7e150f23edd1
MariaJL <mariajr.lisboa@gmail.com>
2022-05-08 22:41:22 -0300

    requisito 85 a 87 - Profile concluído

--- src/pages/14 Profile/Profile.js
+++ src/pages/14 Profile/Profile.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header2';
 
 function Profile() {
   const email = localStorage.getItem('user');
+  const history = useHistory();
 
   return (
     <div>
       <Header PageTitle="Profile" />
       <section>
         <p data-testid="profile-email">{ email }</p>
-        <button type="button" data-testid="profile-done-btn">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
           Done Recipes
         </button>
-        <button type="button" data-testid="profile-favorite-btn">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
           Favorite Recipes
         </button>
-        <button type="button" data-testid="profile-logout-btn">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
           Logout
         </button>
       </section>
